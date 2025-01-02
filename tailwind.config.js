e7d95cfce866885b8ae45dd5beb59cc31e8d50ff
unknown <sujoydaspc2023@gmail.com>
2025-01-02 21:54:34 +0600

    first Commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    mode: 'jit', // Enables JIT mode for Tailwind CSS
+    content: [
+      './src/**/*.{html,js,ts,jsx,tsx}', // Path to your files
+    ],
+  };
+  
